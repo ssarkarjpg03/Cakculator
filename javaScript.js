20d0c027cb3bbb71661aac48dad33bc9cc660629
Subhashis Sarkar <ssarkarjpg03@gmail.com>
2025-01-20 20:34:40 +0530

    some update

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,17 @@
 // Appends values to the display screen
 function number(value) {
     const display = document.getElementById('display');
-    display.innerText += value;
+    display.value += value;
 }
 
 // Clears the display screen
 function clearDisplay() {
     const display = document.getElementById('display');
-    display.innerText = '';
+    display.value = '';
 }
 
 // Calculates the result of the expression
 function calculateResult() {
     const display = document.getElementById('display');
-    display.innerText = eval(display.innerText);
+    display.value = eval(display.value);
 }
